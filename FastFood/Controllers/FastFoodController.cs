58d249c5f6ef850677e19d363106f3b26aa48fd9
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-03-14 06:08:15 +0100

    Adding of rest client for testing requests

--- FastFood/Controllers/FastFoodController.cs
+++ FastFood/Controllers/FastFoodController.cs
@@ -8,30 +8,28 @@ using FastFood.Contracts.FastFood;
 namespace FastFood.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
     public class FastFoodController : ControllerBase
     {
-        
 
         [HttpPost("/fastfood")]
         public IActionResult CreateFastFood(CreateFastFoodRequest request){
             return Ok(request);
         }
 
-        [HttpGet("/fastfood/{guid:id}")]
+        [HttpGet("/fastfood/{id:guid}")]
         public IActionResult GetFastFood(Guid Id){
             return Ok(Id);
         }
 
 
-        [HttpPut("/fastfood/{guid:id}")]
-        public IActionResult UpsertFastFoodRequest(Guid Id, UpsertFastFoodResquest request){
+        [HttpPut("/fastfood/{id:guid}")]
+        public IActionResult UpsertFastFood(Guid Id, UpsertFastFoodResquest request){
             return Ok(request);
         }
 
-        [HttpPut("/fastfood/{guid:id}")]
-        public IActionResult DeleteFastFood(Guid Id, UpsertFastFoodResquest request){
-            return Ok(request);
+        [HttpPut("/fastfood/{id:guid}")]
+        public IActionResult DeleteFastFood(Guid Id){
+            return Ok(Id);
         }
     }
 }
