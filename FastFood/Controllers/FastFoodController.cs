9c9ec8221620197f16dab34f0592692e7968c52f
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-03-14 06:52:53 +0100

    update dynamic endpoint

--- FastFood/Controllers/FastFoodController.cs
+++ FastFood/Controllers/FastFoodController.cs
@@ -1,33 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FastFood.Contracts.FastFood;
 
 namespace FastFood.Controllers
 {
     [ApiController]
+    [Route("/fastfood")]
     public class FastFoodController : ControllerBase
     {
 
-        [HttpPost("/fastfood")]
+        [HttpPost()]
         public IActionResult CreateFastFood(CreateFastFoodRequest request){
             return Ok(request);
         }
 
-        [HttpGet("/fastfood/{id:guid}")]
+        [HttpGet("{id:guid}")]
         public IActionResult GetFastFood(Guid Id){
             return Ok(Id);
         }
 
-
-        [HttpPut("/fastfood/{id:guid}")]
+        [HttpPut("{id:guid}")]
         public IActionResult UpsertFastFood(Guid Id, UpsertFastFoodResquest request){
             return Ok(request);
         }
 
-        [HttpDelete("/fastfood/{id:guid}")]
+        [HttpDelete("{id:guid}")]
         public IActionResult DeleteFastFood(Guid Id){
             return Ok(Id);
         }
