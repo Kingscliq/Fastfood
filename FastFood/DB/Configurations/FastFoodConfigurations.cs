fc7a81603014e7b461a4a0c3639ad39c0188347a
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-03-19 13:53:53 +0100

    Config for max length for name | Config for MaxDescriptionLength | as well as Hasconversion fot Sweet

--- FastFood/DB/Configurations/FastFoodConfigurations.cs
+++ FastFood/DB/Configurations/FastFoodConfigurations.cs
@@ -6,7 +6,14 @@ namespace FastFood.DB.Configurations;
 
 public class FastFoodConfigurations : IEntityTypeConfiguration<FastFoodModel>{
 public void Configure(EntityTypeBuilder<FastFoodModel> builder ) {
+    builder.Property(b => b.Name).HasMaxLength(FastFoodModel.MaxNameLength);
+    builder.Property(b => b.Description).HasMaxLength(FastFoodModel.MaxDescriptionLength);
+
     builder.Property(b => b.Savory).HasConversion(
+        v => string.Join(',', v), 
+        v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
+
+    builder.Property(b => b.Sweet).HasConversion(
         v => string.Join(',', v), 
         v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
 }
