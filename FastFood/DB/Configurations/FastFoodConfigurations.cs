4932b50fa9dd7938ae466a656ba2ed8f395ce1ce
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-03-19 13:44:03 +0100

    add: DB conversions of arrays from and to the DB

--- FastFood/DB/Configurations/FastFoodConfigurations.cs
+++ FastFood/DB/Configurations/FastFoodConfigurations.cs
@@ -5,10 +5,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace FastFood.DB.Configurations;
 
 public class FastFoodConfigurations : IEntityTypeConfiguration<FastFoodModel>{
-
-
-
 public void Configure(EntityTypeBuilder<FastFoodModel> builder ) {
-
+    builder.Property(b => b.Savory).HasConversion(
+        v => string.Join(',', v), 
+        v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
 }
 }
